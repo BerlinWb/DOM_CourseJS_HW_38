45849234081f3260afcfaba847606bb57669232d
Oleh Svetlov <95508050+BerlinWb@users.noreply.github.com>
2022-09-13 12:20:24 +0300

    Add files via upload

---
+++ DOM_CourseJS_HW_38/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+const generateList = (array) => {
+    const ul = document.createElement('ul');
+
+        array.forEach((el) => {
+            const li = document.createElement('li');
+            let childElement = null;
+            if (Array.isArray(el)) {
+                childElement = generateList(el);
+            } else {
+                childElement = document.createTextNode(el);
+            }
+            li.appendChild(childElement);
+            ul.appendChild(li);
+        })
+
+        return document.body.appendChild(ul);
+
+};
+
+generateList([1, 2, 3]);
+generateList([1, 2, [1.1, 1.2, 1.3], 3]);
+generateList([1, 2, [1, 2, [1, 2], 1.3], 3]);
